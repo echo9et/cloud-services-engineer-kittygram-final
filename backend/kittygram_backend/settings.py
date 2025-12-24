1d2846b76fef476aef29e4ee00c9e22ff12d9170
echo9et <echo9et@yandex.ru>
2025-12-24 11:38:38 +0300

    add worflows

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -56,10 +56,14 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "kittygram"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("POSTGRES_HOST", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    },
 }
 
 
