3860a4afa91936f333bc486efc15418a8aee569b
echo9et <echo9et@yandex.ru>
2025-12-25 07:49:44 +0300

    fix: settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "secret-key")
 
-DEBUG = os.getenv("DJANGO_DEBUG", false)
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ['*']
 
